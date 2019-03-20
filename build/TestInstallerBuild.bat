c9016092759a3e874223f8f5a16543fbbea18b30
Hasso <hasso_pape@sil.org>
2019-03-20 15:01:55 -0500

    Update installer (.NET 4.6.1, WiX 3.11, Visual Studio 2017)
    - Change manufacturer to SIL International
    - Check for .NET framework 4.6.1 or later
    - Change WiX hint paths to %WIX%
    - Use vswhere to locate Visual Studio in the test installer script
    - Pass the product version as a parameter to the WiX build

--- build/TestInstallerBuild.bat
+++ build/TestInstallerBuild.bat
@@ -1,4 +1,9 @@
-call "c:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
+setlocal
+@set PATH=C:\Program Files (x86)\Microsoft Visual Studio\Installer;%PATH%
+for /f "usebackq delims=" %%i in (`vswhere -latest -requires Microsoft.Component.MSBuild -property installationPath`) do (
+	set InstallDir=%%i
+)
+call "%InstallDir%\VC\Auxiliary\Build\vcvarsall.bat" x86
 
 pushd .
 MSbuild /target:installer /property:teamcity_build_checkoutDir=..\  /property:teamcity_dotnet_nunitlauncher_msbuild_task="notthere" /property:BUILD_NUMBER="*.*.6.789" /property:Minor="1"
